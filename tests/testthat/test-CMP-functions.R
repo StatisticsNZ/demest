8f4c9f07ef4d41b0b612833b12db42da69406b3f
ctaglioni <ctaglioni@github.com>
2017-11-02 16:51:46 +1300

    test for density 'logDensCMP1' , new test function 'mydcmp'

--- tests/testthat/test-CMP-functions.R
+++ tests/testthat/test-CMP-functions.R
@@ -6,24 +6,25 @@ test.identity <- FALSE
 test.extended <- TRUE
 
 
-test_that("dqcmp1 works", {
-    dqcmp <- demest:::dqcmp
-    q.pdf<- function(y,gamma,nu, log=FALSE){
-        if(log==FALSE){
-            fact <- factorial(y)
-            (gamma^y/fact)^nu
-        } else {
-            nu*(y*log(gamma)-(sum(log(1:y))))
-        }
+test_that("logDensCMP1 works", {
+  logDensCMP1 <- demest:::logDensCMP1
+    
+    mydcmp<- function(y,gamma,nu,log=FALSE){
+
+      pdf <- nu*(y*log(gamma)-lgamma(y+1))
+      
+      if(log==FALSE){ pdf <- exp(pdf)}
+      
+      return(pdf)
     }
+
     for (seed in seq_len(n.test)) {
         set.seed(seed)
         y <- rpois(n = 1, lambda = 10)
         gamma <- runif(n = 1, max = 10)
         nu <- runif(n = 1, max = 10)
-        log <- runif(n = 1) < 0.5
-        ans.obtained <- dqcmp1(x = x, gamma = gamma, nu = nu, log = log)
-        ans.expected <- q.pdf(x = x, gamma = gamma, nu = nu, log = log)
+        ans.obtained <- logDensCMP1(x = x, gamma = gamma, nu = nu)
+        ans.expected <- mydcmp(x = x, gamma = gamma, nu = nu, log = TRUE)
         if (test.identity)
             expect_identical(ans.obtained, ans.expected)
         else
